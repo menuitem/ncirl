0990d7fb7f8ad49920cca31da9fb6f2c968e4d74
menuitem <mishmash111@gmail.com>
2014-03-22 17:22:23 +0000

    grid

--- www/JasmineLab/js/main.js
+++ www/JasmineLab/js/main.js
@@ -6,7 +6,7 @@ window._skel_config = {
 		grid: { gutters: 30 },
 		breakpoints: {
 		wide: { range: '1200-', containers: 1140, grid: { gutters: 50 } },
-		narrow: { range: '481-1199', containers: 'fluid' },
+		narrow: { range: '481-1199', containers: 900 },
 		mobile: { range: '-480', containers: 'fluid', lockViewport: true, grid: { collapse: true } }
 	  }
   };
